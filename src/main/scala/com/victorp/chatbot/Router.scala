632ed545d9abe2ef49bd19ffa00456c76be73952
Victor Perepelitsky <victor.prp@gmail.com>
2016-12-19 21:35:28 +0200

    logback is working

--- src/main/scala/com/victorp/chatbot/Router.scala
+++ src/main/scala/com/victorp/chatbot/Router.scala
@@ -1,19 +1,19 @@
 package com.victorp.chatbot
 
 import akka.actor.{ActorRef, Actor}
+import com.victorp.chatbot.actor.BaseActor
 import com.victorp.chatbot.dto.ChatMessage
 
 /**
  * @author victorp
  */
-class Router extends Actor{
+class Router extends BaseActor{
 
   val chatManagers:Map[String,ActorRef] = Map()
 
   override def receive: Receive = {
     case chatMsg: ChatMessage  => {
-      System.out.println("Router recieved msg:" + chatMsg)
-      System.exit(0)
+      log.debug(s"Router received msg: {}",chatMsg)
     }
   }
 }
---
+++ src/main/scala/com/victorp/chatbot/actor/BaseActor.scala
@@ -0,0 +1,10 @@
+package com.victorp.chatbot.actor
+
+import akka.actor.{ActorLogging, Actor}
+
+/**
+ * @author victorp
+ */
+abstract class BaseActor extends Actor with ActorLogging{
+
+}
