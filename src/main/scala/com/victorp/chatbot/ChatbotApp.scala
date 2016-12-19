c071e62feddd64f28af8fccbebc88a8740a4ed17
Victor Perepelitsky <victor.prp@gmail.com>
2016-12-19 17:51:25 +0200

    cleanup

--- src/main/scala/com/victorp/chatbot/ChatbotApp.scala
+++ src/main/scala/com/victorp/chatbot/ChatbotApp.scala
@@ -25,14 +25,5 @@ object ChatbotApp extends App{
   val router = system.actorOf(Props[Router])
 
   router ! ChatMessage("user-1","Hello Bot!")
-//  val bigList =
-//    for {
-//      i <- 1 to 100000
-//    } yield Random.nextInt()
-//
-//
-//  val fmt = new SimpleDateFormat("ss-mm-hh")
-//  println(s"""start sorting now ${fmt.format(new GregorianCalendar().getTime)} """)
-//  mergeSortActor ! SortItPlease(bigList.toList,userFacadeActor)
 
 }
